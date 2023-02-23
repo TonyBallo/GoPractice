efc010d8925d8f9695f6a822d6320afc4d9fae18
TonyBallo <acballo77@gmail.com>
2023-02-22 19:43:38 -0700

    signup added 2/22/23 7:43

--- slamps/dummyDatabase.go
+++ slamps/dummyDatabase.go
@@ -5,13 +5,29 @@ import (
 	"strings"
 )
 
+// Profile structure for people 
+type Profile struct {
+	firstName string
+	lastName string
+	username string
+	password string
+}
+
 func main () {
-	
+	// create the database map
+	db := make(map[int]Profile)
+	db[1] = Profile{
+		firstName: "ant",
+		lastName: "ball",
+		username: "user",
+		password: "pass",
+	}
 	switch input := strings.ToUpper(Login_o_Signup()); input {
 	case "S":
-		fmt.Println("Signing up...")
+		id := Signup(db)
+		fmt.Printf("\nProfile Created, Your ID is %d", id)
 	case "L":
-		fmt.Println("Logging in...")
+		Login()
 	default:
 		fmt.Println("Something is wrong...")
 	}
@@ -29,6 +45,49 @@ func Login_o_Signup() string {
 	}
 
 	return input
+}
+
+func Signup(database map[int]Profile) int {
+
+	var username, password, fname, lname string
+
+	// ask for data to make profile
+	fmt.Println("##### Lets Get You Signed Up! #####\n\n")
+	fmt.Println("Username : >")
+	fmt.Scan(&username)
+	fmt.Println("Password : >")
+	fmt.Scan(&password)
+	fmt.Println("First Name : >")
+	fmt.Scan(&fname)
+	fmt.Println("Last Name : >")
+	fmt.Scan(&lname)
+
+	// initialize new Profile with given data
+	newProfile := Profile{
+		username: username, 
+		password: password, 
+		firstName: fname, 
+		lastName: lname,
+	}
+
+	// make id for profile
+	var newID int 
+	if len(database) != 0 {
+		newID = len(database) + 1
+	} else {
+		newID = 0
+	}
+
+	// add profile to database
+	database[newID] = newProfile
+
+	// return the new id for made profile
+	return newID
+	
+}
+
+func Login() {
+
 }
 
 /* Outline 
